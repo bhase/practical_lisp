03a9ab12d95aee24a209039d5b3ed5625f840582
Benjamin Hase <benjamin.hase@web.de>
2014-09-15 16:58:02 +0200

    add remove-rows function

--- simple-database.lisp
+++ simple-database.lisp
@@ -58,3 +58,6 @@
                 (if rating   (setf (getf row :rating) rating))
                 (if ripped-p (setf (getf row :ripped) ripped)))
               row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
