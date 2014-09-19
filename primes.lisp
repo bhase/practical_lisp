068fb681f00945982d7c1ed818dbcbc4fb09b033
Benjamin Hase <benjamin.hase@web.de>
2014-09-19 15:46:14 +0200

    add macro examples: do-primes (chapter 8)

---
+++ primes.lisp
@@ -0,0 +1,11 @@
+(defun primep (num)
+  (when (> num 1)
+    (loop for fac from 2 to (isqrt num) never (zerop (mod num fac)))))
+
+(defun next-prime (num)
+  (loop for n from num when (primep n) return n))
+
+(defmacro do-primes ((var start end) &body body)
+  `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+        ((> ,var ,end))
+        ,@body))
